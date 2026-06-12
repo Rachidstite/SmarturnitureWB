2557aa62f1fbc3c447c95aef5ffaad2b4ce87159
Rachid <rachidstite4@gmail.com>
2026-06-12 21:40:23 +0100

    add manufacturing cost rules contract

---
+++ cost_intelligence/manufacturing_cost_rules.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class ManufacturingCostRules:
+    material_area_rate: float = 120.0
+    edge_meter_rate: float = 5.0
+    drilling_rate: float = 1.5
+    complexity_material_type_rate: float = 25.0
+    currency: str = "MAD"
