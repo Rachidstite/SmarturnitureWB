a3cb93d7ec40bd027041432aee388b5f4c3fa97e
Rachid <rachidstite4@gmail.com>
2026-06-20 17:33:51 +0100

    Add material availability decision contract

---
+++ manufacturing/material_availability_decision_report.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class MaterialAvailabilityDecisionReport:
+    decision_status: str = ""
+    is_manufacturable: bool = False
+    is_blocked: bool = False
+    requires_review: bool = False
+    blocking_reason: str = ""
+    warning_reason: str = ""
+    recommended_fix: str = ""
+    material_availability_status: str = ""
+    factory_visibility_message: str = ""
